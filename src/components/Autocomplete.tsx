46f6e5e84b949a695b3b9fda5ce8221d3c918ed5
Marika Shanahan <marikashanahan@gmail.com>
2023-10-18 20:12:24 -0600

    Configure Tailwind css

---
+++ src/components/Autocomplete.tsx
@@ -0,0 +1,11 @@
+const Autocomplete = () => {
+   
+    return (
+    <>
+        <label htmlFor="searchInput">Country</label>
+        <input id="searchInput" type="text" placeholder="Search for a movie" className="search-bar" />
+    </>
+    )
+}
+
+export default Autocomplete;
