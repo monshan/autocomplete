010453a7d1e99dea8d9a10750c66d1e892f47496
Marika Shanahan <marikashanahan@gmail.com>
2023-10-19 01:11:06 -0600

    Working debounce with loading state
    Implement debounce with setTimeout in the useEffect hook

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import Autocomplete from './components/Autocomplete';
 
 function App() {
   return (
-    <div className="grid xl:grid-cols-12 grid-cols-8 gap-2 w-screen h-screen">
-      <Autocomplete labelName="Country" />
+    <div className="grid xl:grid-cols-12 grid-cols-8 gap-2 w-screen h-screen bg-[radial-gradient(ellipse_at_top_left,_var(--tw-gradient-stops))] from-rose-400 to-orange-300">
+      <Autocomplete />
     </div>
   );
 }
--- src/components/Autocomplete.tsx
+++ src/components/Autocomplete.tsx
@@ -1,8 +1,4 @@
-import React, {useState, useEffect} from "react";
-
-interface AutocompleteProps {
-    labelName: string;
-}
+import React, { useState, useEffect } from "react";
 
 interface CountryName {
     common: string;
@@ -15,27 +11,49 @@ interface Country {
 
 }
 
-const Autocomplete: React.FC<AutocompleteProps> = ({ labelName }) => {
-    const [contries, setCountries] = useState<Country[]>([])
+const Autocomplete: React.FC = () => {
+    const [searchTerm, setSearchTerm] = useState<string>('')
+    const [countries, setCountries] = useState<Country[]>([])
+    const [loading, setLoading] = useState<boolean>(false)
+    const [currentTimer, setCurrentTimer] = useState<ReturnType<typeof setTimeout>>();
 
     useEffect(() => {
-        const fetchCountryMatches = async () => {
-            const response = await fetch(`https://restcountries.com/v3.1/name/united?fields=name,flag`)
-            const data = await response.json()
-            setCountries(data)
+        if (currentTimer) {
+            clearTimeout(currentTimer)
         }
 
-        fetchCountryMatches();
-    }, [])
-   
+        const fetchCountryMatches = async () => {
+            const response = await fetch(`https://restcountries.com/v3.1/name/${searchTerm}?fields=name,flag`)
+            const data = await response.json()
+            setCountries(data)
+            setLoading(false)
+        }
+
+        if (searchTerm) {    
+            let debouceTimer: ReturnType<typeof setTimeout> = setTimeout(() => {
+                fetchCountryMatches();
+            }, 1000)
+
+            setLoading(true)
+            setCurrentTimer(debouceTimer);
+        }
+    }, [searchTerm])
+
     return (
-    <>
-        <label htmlFor="searchInput" className="xl:col-start-5 col-start-4 self-end">{labelName}</label>
-        <input id="searchInput" type="text" placeholder="Search for a country" className="xl:col-span-4 col-span-3 self-end" />
-        <ol className="row-start-2 xl:col-start-6 col-start-7 xl:col-span-4 col-span-3">
-            { contries.map((country) => <li>{country.flag} {country.name.common} </li>)}
-        </ol>
-    </>
+        <>
+        {/* <div className="w-full h-3 bg-gray-300 rounded-full overflow-hidden">
+            <div id="loading-bar" className="h-full bg-blue-500 transition-width ease-in-out duration-2000" style={{ width: 50 }}></div>
+        </div> */}
+        <div className="bg-white xl:col-start-5 col-start-3 xl:col-span-5 col-span-4 self-center rounded-2xl py-4 px-4 shadow-lg grid grid-cols-4 relative">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 self-center">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+            </svg>
+            <input id="searchInput" type="text" placeholder="Search for a country" className="col-span-3" onChange={(e) => setSearchTerm(e.target.value)}/>
+            { searchTerm && <ol className="shadow-2xl bg-white absolute left-24 inset-x-0 top-16 max-h-48 overflow-y-auto">
+                { loading ? <li className="p-3">Loading...</li> : countries.length && countries.map((country) => <li className="p-3 hover:bg-blue-600 hover:text-white truncate">{country.flag} {country.name.common}</li>)}
+            </ol>}
+        </div>
+        </>
     )
 }
 
